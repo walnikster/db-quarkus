942a89d3da5fe675a4fa690c23194f323f205fc8
niki <niki@theliferocks.com>
2023-11-24 18:59:39 +0200

    'reformatting'

--- customer/src/main/java/com/uleos/cyprus/jpa/Customer.java
+++ customer/src/main/java/com/uleos/cyprus/jpa/Customer.java
@@ -1,9 +1,6 @@
 package com.uleos.cyprus.jpa;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,10 +15,16 @@ public class Customer {
     @GeneratedValue
     private Long id;
 
+    @Column(name = "first_name", length = 50, nullable = false)
     private String firstName;
+
+    @Column(name = "last_name", length = 50, nullable = false)
     private String lastName;
 
+    @Column(name = "email", length = 255, nullable = false)
     private String email;
+
+    @Column(name = "created_date", nullable = false)
     private Instant created = Instant.now();
 
     public Customer() {
